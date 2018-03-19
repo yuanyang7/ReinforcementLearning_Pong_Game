fd64c2b2595800238cad76171c07ebdf1a837c16
wangkepfe <wangkepfe@gmail.com>
2018-03-19 03:31:08 -0700

    addsomethings

--- deep_q_network_2.py
+++ deep_q_network_2.py
@@ -44,10 +44,10 @@ def createNetwork():
     W_conv3 = weight_variable([3, 3, 64, 64])
     b_conv3 = bias_variable([64])
     
-    W_fc1 = weight_variable([1600, 512])
-    b_fc1 = bias_variable([512])
+    W_fc1 = weight_variable([256, 256])
+    b_fc1 = bias_variable([256])
 
-    W_fc2 = weight_variable([512, ACTIONS])
+    W_fc2 = weight_variable([256, ACTIONS])
     b_fc2 = bias_variable([ACTIONS])
 
     # input layer
@@ -57,7 +57,7 @@ def createNetwork():
     h_conv1 = tf.nn.relu(conv2d(s, W_conv1, 4) + b_conv1)
     h_conv2 = tf.nn.relu(conv2d(h_conv1, W_conv2, 2) + b_conv2)
     h_conv3 = tf.nn.relu(conv2d(h_conv2, W_conv3, 1) + b_conv3)
-    h_conv3_flat = tf.reshape(h_conv3, [-1, 1600])
+    h_conv3_flat = tf.reshape(h_conv3, [-1, 256])
 
     h_fc1 = tf.nn.relu(tf.matmul(h_conv3_flat, W_fc1) + b_fc1)
 
@@ -100,7 +100,7 @@ def trainNetwork(s, readout, h_fc1, sess):
     epsilon = INITIAL_EPSILON
 
     # observe state
-    for t in range(OBSERVE):
+    for t in range(int(OBSERVE)):
         # choose an action epsilon greedily
         readout_t = readout.eval(feed_dict = {s : [s_t]})[0]
         a_t = np.zeros([ACTIONS])
